92dbe8b372900f040b90a501b637f2dd2429c2a8
Saraxpr <120026819+Saraxpr@users.noreply.github.com>
2025-05-25 13:02:15 +0200

    Update index.blade.php

--- banka/resources/views/departments/index.blade.php
+++ banka/resources/views/departments/index.blade.php
@@ -1,17 +1,32 @@
 <!-- resources/views/departments/index.blade.php -->
 @extends('layouts.app')
-<h1>Lista Departmana</h1>
-<a href="{{ route('departments.create') }}">Dodaj novi departman</a>
-<ul>
-    @foreach ($departments as $department)
-        <li>
-            {{ $department->NAME }}
-            <a href="{{ route('departments.edit', $department->DEPT_ID) }}">Uredi</a>
-            <form action="{{ route('departments.destroy', $department->DEPT_ID) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Obriši</button>
-            </form>
-        </li>
-    @endforeach
-</ul>
+
+@section('content')
+<div class="container">
+    <h1 class="mb-4">Lista Departmana</h1>
+    <a href="{{ route('departments.create') }}" class="btn btn-primary mb-4">Dodaj novi departman</a>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Naziv Departmana</th>
+                <th>Akcije</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($departments as $department)
+            <tr>
+                <td>{{ $department->NAME }}</td>
+                <td>
+                    <a href="{{ route('departments.edit', $department->DEPT_ID) }}" class="btn btn-warning btn-sm">Uredi</a>
+                    <form action="{{ route('departments.destroy', $department->DEPT_ID) }}" method="POST" style="display: inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Obriši</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
