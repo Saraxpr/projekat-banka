51687010a84a1fdd103f04599ae7bbbe78ee3902
Saraxpr <120026819+Saraxpr@users.noreply.github.com>
2025-06-03 15:23:24 +0200

    Update Department.php

--- banka/app/Models/Department.php
+++ banka/app/Models/Department.php
@@ -3,11 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OpenApi\Annotations as OA;
 
+/** 
+ * @OA\Schema(
+ * title="Department",
+ * description="Department model",
+ * @OA\Xml(
+ * name="Department"
+ * ),
+ * @OA\Property(
+ * property="DEPT_ID", 
+ * title="ID",
+ * description="ID of the department",
+ * type="integer",
+ * format="int64",
+ * example=1
+ * ),
+ * @OA\Property(
+ * property="NAME", 
+ * title="Name",
+ * description="Name of the department",
+ * type="string",
+ * example="IT Department"
+ * )
+ * )
+ */
 class Department extends Model
 {
-    protected $table = 'department';  // Definirajte naziv tablice ako nije uobičajeni oblik
+    protected $table = 'department';
     protected $primaryKey = 'DEPT_ID';
+
     protected $fillable = ['NAME'];
-    public $timestamps = false;  // Ovdje onemogućavamo automatske timestamps ako ne postoje
+
+    public $timestamps = false;
 }
